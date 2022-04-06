4baba33e736cf6cb774fb88f9e51949863ab1c5a
laarkell <70282901+laarkell@users.noreply.github.com>
2022-04-05 21:36:15 -0400

    Update getapi.py

--- getapi.py
+++ getapi.py
@@ -1,15 +1,14 @@
-$ pip3 install libtad
 
 from libtad import AstronomyService
-    from libtad.datatypes.places import Coordinates, LocationId
-    from libtad.datatypes.time import TADDateTime
-    from libtad.datatypes.astro import AstronomyEventClass, AstronomyObjectType
+from libtad.datatypes.places import Coordinates, LocationId
+from libtad.datatypes.time import TADDateTime
+from libtad.datatypes.astro import AstronomyEventClass, AstronomyObjectType
     
-    coordinates = Coordinates(59.743, 10.204)
-    place = LocationId(coordinates)
-    date = TADDateTime(2020, 11, 26)
-    service = AstronomyService("sdV4VfLW1J", "urvp9Ka73g6q7BZ0Fd4U")
-    service.types = AstronomyEventClass.Meridian | AstronomyEventClass.Phase
+coordinates = Coordinates(59.743, 10.204)
+place = LocationId(coordinates)
+date = TADDateTime(2020, 11, 26)
+service = AstronomyService("sdV4VfLW1J", "urvp9Ka73g6q7BZ0Fd4U")
+service.types = AstronomyEventClass.Meridian | AstronomyEventClass.Phase
 
 astro_info = service.get_astronomical_info(AstronomyObjectType.Moon, place, date)
     
