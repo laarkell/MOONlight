9494a309388c22f1bb283eef222839402ad379f7
laarkell <70282901+laarkell@users.noreply.github.com>
2022-05-04 15:52:53 -0400

    Update moonlight.py

--- moonlight.py
+++ moonlight.py
@@ -1,1 +1,70 @@
+# https://peppe8o.com/download/python/8x8LedDisplay.py
+
+import RPi.GPIO as GPIO
+import sys
+
+#define PINs according to cabling
+columnDataPin = 20
+rowDataPin = 21
+latchPIN = 14
+clockPIN = 15
+
+#set pins to putput
+GPIO.setmode(GPIO.BCM)
+GPIO.setup((columnDataPin,rowDataPin,latchPIN,clockPIN),GPIO.OUT)
+
+#define shift register update function
+def shift_update_matrix(input_Col,Column_PIN,input_Row,Row_PIN,clock,latch):
+  #put latch down to start data sending
+  GPIO.output(clock,0)
+  GPIO.output(latch,0)
+  GPIO.output(clock,1)
+
+  #load data in reverse order
+  for i in range(15, -1, -1):
+    GPIO.output(clock,0)
+    #instead of controlling only 1 shift register, we drive both together
+    GPIO.output(Column_PIN, int(input_Col[i]))
+    GPIO.output(Row_PIN, int(input_Row[i]))
+    GPIO.output(clock,1)
+
+  #put latch up to store data on register
+  GPIO.output(clock,0)
+  GPIO.output(latch,1)
+  GPIO.output(clock,1)
+
+#map your output into 1 (LED off) and 0 (led on) sequences
+
+fullMoon = [["0000001111000000"],\
+            ["0000111111110000"],\
+            ["0001111111111000"],\ 
+            ["0011111111111100"],\ 
+            ["0111111111111110"],\ 
+            ["0111111111111110"],\ 
+            ["1111111111111111"],\
+            ["1111111111111111"],\
+            ["1111111111111111"],\
+            ["1111111111111111"],\
+            ["0111111111111110"],\ 
+            ["0111111111111110"],\ 
+            ["0011111111111100"],\
+            ["0001111111111000"],\ 
+            ["0000111111110000"],\
+            ["0000001111000000"]]
+
+#main program, calling shift register function to activate 16x16 LED Matrix
+while True:
+  try:
+    RowSelect=[1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+    for i in range(0,16): # last value in rage is not included by default
+      # send row data and row selection to registers
+      shift_update_matrix(''.join(map(str, fullMoon[i])),columnDataPin,\
+                          ''.join(map(str, RowSelect)),rowDataPin,clockPIN,latchPIN)
+      #shift row selector
+      RowSelect = RowSelect[-1:] + RowSelect[:-1]
+
+#PINs final cleaning on interrupt
+  except KeyboardInterrupt:
+    GPIO.cleanup()
+    sys.exit()
 
