7eaa779f1dcb725049f8021f5fce507363fa98d3
laarkell <70282901+laarkell@users.noreply.github.com>
2022-05-09 09:48:49 -0400

    Delete apimoon.py

--- apimoon.py
+++
@@ -1,7 +0,0 @@
-# https://www.askpython.com/python/examples/pull-data-from-an-api
-# https://ipgeolocation.io/documentation/astronomy-api.html
-
-response_API = get('https://api.ipgeolocation.io/astronomy?apiKey=09c8f79c7db149f7ad8d42ffa7a73ce2&lat=35.499&long=-80.848')
-data = response_API.text
-print(data)
-
