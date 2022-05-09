cf525cefccd6a1832d6dfa63860cae06409d7421
laarkell <70282901+laarkell@users.noreply.github.com>
2022-05-08 21:38:47 -0400

    Update moonrn.py

--- moonrn.py
+++ moonrn.py
@@ -1,5 +1,6 @@
 import math, decimal, datetime
 import sys
+import RPi.GPIO as GPIO
 from datetime import datetime, timedelta
 import time
 import calendar
@@ -367,6 +368,6 @@ if moonrise1 > rntime and moonset1 < rntime:
          RowSelect = RowSelect[-1:] + RowSelect[:-1]
 
 #PINs final cleaning on interrupt
-   except KeyboardInterrupt:
-     GPIO.cleanup()
-     sys.exit()
+     except KeyboardInterrupt:
+       GPIO.cleanup()
+       sys.exit()
